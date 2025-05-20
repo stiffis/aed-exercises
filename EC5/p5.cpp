6271127b4f3c83aed962b01ecb94ac43612dc97b
stiffis <steve.ildefonso@utec.edu.pe>
2025-05-20 09:41:20 -0500

    feat: add exercise

---
+++ EC5/p5.cpp
@@ -0,0 +1,27 @@
+/*
+ * l1
+ * l2
+ * l3
+ * int dim = 0;
+ * auto it = l1.begin();
+ * while(it != l1.end()) {
+ *   dim ++;
+ *   it++;
+ * }
+ * tipofinal = {}
+ * auto it1 = l1.begin();
+ * auto it2 = l2.begin();
+ * auto it3 = l3.begin();
+ * while(it1 != l1.end()) {
+ *   vector<int> temp;
+ *   temp.push_back(*it1);
+ *   temp.push_back(*it2);
+ *   temp.push_back(*it3);
+ *   tipofinal.push_back(temp);
+ *   it1++;
+ *   it2++;
+ *   it3++;
+ * }
+ *
+ *
+ */
